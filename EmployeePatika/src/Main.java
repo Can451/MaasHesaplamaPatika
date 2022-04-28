fccc94295bfb376339b5bfc1f2ce91f03869d1c4
Can Yeşiltepe <cyesiltepe9@gmail.com>
2022-04-28 15:57:29 +0300

    MaasHesaplama

---
+++ EmployeePatika/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        Employee w1=new Employee("Can",5000,45,2005);
+        w1.ToString();
+        Employee w2=new Employee("Emir Ali",6000,40,2010);
+        w2.ToString();
+        Employee w3= new Employee("Mert",10000,25,2020);
+        w3.ToString();
+
+
+
+
+
+
+
+    }
+}
